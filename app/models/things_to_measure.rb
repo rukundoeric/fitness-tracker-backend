25ac6ec1220a63866b7c845d4b0ef2a1c26826b3
Rukundo Eric <ericrukundo005@gmail.com>
2021-04-13 22:25:43 +0200

    Migrate database

--- app/models/things_to_measure.rb
+++ app/models/things_to_measure.rb
@@ -1,6 +1,7 @@
 class ThingsToMeasure < ApplicationRecord
   belongs_to :user
   validates :name, :unit, :user_id, presence: true
+  validates :name, uniqueness: true
 
   scope :eager_loading, -> { includes(:user) }
 end
