b315d0c94fc0aba490bec89f13bddd7b33c0669a
Rukundo Eric <ericrukundo005@gmail.com>
2021-04-19 16:16:43 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  post 'sign_up' => 'user#create', as: 'sign_up'
-  post 'sign_in' => 'auth#create', as: 'sign_in'
-
-  resources :user, only: %i[index show update]
+  resourses :auth, only: %i[create]
+  resources :user, only: %i[index show update create]
   resources :things_to_measure, only: %i[index create destroy show]
   resources :measurement, only: %i[index create show destroy]
   resources :progress, only: %i[index]
